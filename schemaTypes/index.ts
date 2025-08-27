54e3199bbb323d6f77dd19112acb6d1c7bff7489
Jen <evunti@gmail.com>
2025-08-27 14:23:05 -0400

    added next and fixed bun

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,1 +1,29 @@
-export const schemaTypes = []
+import {type SchemaTypeDefinition} from 'sanity'
+
+const photoAlbumSchema = {
+  name: 'photoAlbum',
+  type: 'document',
+  title: 'Photo Album',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Album Title',
+    },
+    {
+      name: 'description',
+      type: 'text',
+      title: 'Album Description',
+    },
+    {
+      name: 'photos',
+      type: 'array',
+      of: [{type: 'image'}],
+      title: 'Photos',
+    },
+  ],
+}
+
+export const schema: {types: SchemaTypeDefinition[]} = {
+  types: [photoAlbumSchema],
+}
