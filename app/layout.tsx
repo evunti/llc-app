13bb1810b780e7aa7d548cb541a3a8ab19fa3ada
Jen <evunti@gmail.com>
2025-08-27 15:53:12 -0400

    added some styling to nav and footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,24 +7,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
-        <nav
-          className=" flex
-         gap-6"
-        >
+      <body className="relative min-h-screen flex flex-col">
+        <nav className="absolute top-0 right-0 mt-6 mr-8 flex gap-6 items-end">
           <div>About</div>
           <div>Gallery</div>
           <div>Contact</div>
         </nav>
-        {children}
+        <div className="flex-1">{children}</div>
       </body>
-      <footer>
+      <footer className="w-full flex justify-center items-center py-6 text-center">
         &copy; {new Date().getFullYear()} Evunti Photography |
         <a
           href="https://instagram.com/zhenya.untilova"
           target="_blank"
           rel="noopener noreferrer"
-          style={{ color: "#405de6", textDecoration: "none", marginLeft: 6 }}
+          className="ml-2 underline"
         >
           Instagram
         </a>
