ea2bc0161ba6af82a676e6d1ec19989ee6b12727
Jen <evunti@gmail.com>
2025-09-24 14:36:29 -0400

    refactor: update repair Job schema and remove photoAlbum schema

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,29 +1,68 @@
-import {type SchemaTypeDefinition} from 'sanity'
+import { type SchemaTypeDefinition } from "sanity";
 
-const photoAlbumSchema = {
-  name: 'photoAlbum',
-  type: 'document',
-  title: 'Photo Album',
+// const photoAlbumSchema = {
+//   name: "photoAlbum",
+//   type: "document",
+//   title: "Photo Album",
+//   fields: [
+//     {
+//       name: "title",
+//       type: "string",
+//       title: "Album Title",
+//     },
+//     {
+//       name: "description",
+//       type: "text",
+//       title: "Album Description",
+//     },
+//     {
+//       name: "photos",
+//       type: "array",
+//       of: [{ type: "image" }],
+//       title: "Photos",
+//     },
+//   ],
+// };
+
+const repairJobSchema = {
+  name: "repairJob",
+  type: "document",
+  title: "Repair Job",
   fields: [
     {
-      name: 'title',
-      type: 'string',
-      title: 'Album Title',
+      name: "title",
+      type: "string",
+      title: "Job Title",
     },
     {
-      name: 'description',
-      type: 'text',
-      title: 'Album Description',
+      name: "description",
+      type: "text",
+      title: "Job Description",
     },
     {
-      name: 'photos',
-      type: 'array',
-      of: [{type: 'image'}],
-      title: 'Photos',
+      name: "beforePhoto",
+      type: "image",
+      title: "Before Photo",
+    },
+    {
+      name: "afterPhoto",
+      type: "image",
+      title: "After Photo",
+    },
+    {
+      name: "date",
+      type: "datetime",
+      title: "Repair Date",
+    },
+    {
+      name: "tags",
+      type: "array",
+      of: [{ type: "string" }],
+      title: "Tags",
     },
   ],
-}
+};
 
-export const schema: {types: SchemaTypeDefinition[]} = {
-  types: [photoAlbumSchema],
-}
+export const schema: { types: SchemaTypeDefinition[] } = {
+  types: [repairJobSchema],
+};
