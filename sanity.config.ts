96f18ae216049e96aafa54ab2021f5b5bbdc71d5
Jen <evunti@gmail.com>
2025-10-09 13:19:19 -0400

    feat: update Sanity configuration and add new schemas
    - Added `next-sanity` package to package.json for improved Sanity integration.
    - Refactored Sanity configuration to use environment variables for project ID and dataset.
    - Updated the default API version in the Sanity configuration.
    - Created new `env.ts` file to manage environment variables for Sanity.
    - Implemented a new client for Sanity in `client.ts` to streamline API interactions.
    - Added image URL builder in `image.ts` for handling images from Sanity.
    - Introduced live content capabilities in `live.ts` for real-time updates.
    - Created a new schema for repair jobs in `index.ts` to manage repair job documents.
    - Established a structure for the Sanity studio in `structure.ts` for better content organization.

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,15 +1,16 @@
 import { visionTool } from "@sanity/vision";
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
-import { schema } from "./schemaTypes";
+import { schema } from "./sanity/schemaTypes/index";
+import { apiVersion, dataset, projectId } from "./sanity/env";
 
-const projectId: string = "3y5ef4tp";
-const dataset: string = "production";
+// const projectId: string = "3y5ef4tp";
+// const dataset: string = "production";
 
 export default defineConfig({
   basePath: "/studio",
   projectId,
   dataset,
   schema,
-  plugins: [structureTool(), visionTool({ defaultApiVersion: "2023-01-01" })],
+  plugins: [structureTool(), visionTool({ defaultApiVersion: "2025-07-14" })],
 });
